99e12e12fbb421befd1025f8daa03ea88d5432d3
leenday <iammilagre@gmail.com>
2020-03-03 21:50:36 +0300

    files after practice on lection

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    root 'welcome#index'
+  end
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :orders do
+    member do
+      get 'approve'
+      get 'calc'
+
+    end
+    collection do
+      get 'first'
+    end
+  end
+
 end
