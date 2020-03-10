49b1d2927dbcd11f27d73742c35f7b61cfccaed1
leenday <iammilagre@gmail.com>
2020-03-10 22:02:24 +0300

    lection networks

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,6 +6,8 @@ class OrdersController < ApplicationController
   # GET /orders.json
   def index
     @orders = Order.all
+
+    @users = User.all
     puts "params: #{params.inspect}"
   end
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ApplicationRecord
+    belongs_to :user
+    has_and_belongs_to_many :tags
 end
+
+# 10.times {|i| Order.create(name: "my_vm#{i}", cost: rand(1...1000))}
